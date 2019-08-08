a56da0162b52e654dda7c3f9c9a09d376a7f2a48
marcuspereiradev <vinnycoast@gmail.com>
2019-08-08 16:53:39 -0300

    Setting API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Api from '../services/Api';
 
 import '../styles/Login.scss';
 
 import logo from '../assets/logo.svg';
 
-class Login extends Component {
-  render() {
-    return (
-      <div className='login-container'>
-        <form>
-          <img src={logo} alt='Tindev' />
-          <input placeholder='Digite seu usuário no GitHub'/>
-          <button type='submit'>Enviar</button>
-        </form>
-      </div>
-    )
+const Login = ({ history }) => {
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await Api.post('/devs', { username });
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
   }
+
+  return (
+    <div className='login-container'>
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt='Tindev' />
+        <input
+          placeholder='Digite seu usuário no GitHub'
+          onChange={event => setUsername(event.target.value)}
+        />
+        <button type='submit'>Enviar</button>
+      </form>
+    </div>
+  )
 };
 
 export default Login;
