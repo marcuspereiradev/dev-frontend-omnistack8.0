f32bea82487616fa8a465c91f5b9288125d8a2f6
marcuspereiradev <vinnycoast@gmail.com>
2019-08-08 16:54:36 -0300

    Creating layout where the devs stay

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,102 @@
 import React from 'react';
 
-const Main = () => {
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+import '../styles/Main.scss';
+
+const Main = ({ match }) => {
   return (
-    <h1>Main!</h1>
+   <div className='main-container'>
+     <img src={logo} alt='Tindev' />
+     <ul>
+       <li>
+         <img src='https://avatars1.githubusercontent.com/u/2723477?v=4' alt='Avatar' />
+         <footer>
+           <strong>Jonas Porto</strong>
+           <p>Curious, self-starter, troubleshooter and team player. \r\nWorking w/ Cloud, Unix, Ruby, Javascript.\r\nExploring data science possibilities w/ Python, R & Ruby</p>
+         </footer>
+
+         <div className='buttons'>
+           <button type='button'>
+             <img src={dislike} alt='Dislike' />
+           </button>
+           <button type='button'>
+             <img src={like} alt='Like' />
+           </button>
+         </div>
+       </li>
+
+       <li>
+         <img src='https://avatars1.githubusercontent.com/u/2723477?v=4' alt='Avatar' />
+         <footer>
+           <strong>Jonas Porto</strong>
+           <p>Curious, self-starter, troubleshooter and team player. \r\nWorking w/ Cloud, Unix, Ruby, Javascript.\r\nExploring data science possibilities w/ Python, R & Ruby</p>
+         </footer>
+
+         <div className='buttons'>
+           <button type='button'>
+             <img src={dislike} alt='Dislike' />
+           </button>
+           <button type='button'>
+             <img src={like} alt='Like' />
+           </button>
+         </div>
+       </li>
+
+       <li>
+         <img src='https://avatars1.githubusercontent.com/u/2723477?v=4' alt='Avatar' />
+         <footer>
+           <strong>Jonas Porto</strong>
+           <p>Curious, self-starter, troubleshooter and team player. \r\nWorking w/ Cloud, Unix, Ruby, Javascript.\r\nExploring data science possibilities w/ Python, R & Ruby</p>
+         </footer>
+
+         <div className='buttons'>
+           <button type='button'>
+             <img src={dislike} alt='Dislike' />
+           </button>
+           <button type='button'>
+             <img src={like} alt='Like' />
+           </button>
+         </div>
+       </li>
+
+       <li>
+         <img src='https://avatars1.githubusercontent.com/u/2723477?v=4' alt='Avatar' />
+         <footer>
+           <strong>Jonas Porto</strong>
+           <p>Curious, self-starter, troubleshooter and team player. \r\nWorking w/ Cloud, Unix, Ruby, Javascript.\r\nExploring data science possibilities w/ Python, R & Ruby</p>
+         </footer>
+
+         <div className='buttons'>
+           <button type='button'>
+             <img src={dislike} alt='Dislike' />
+           </button>
+           <button type='button'>
+             <img src={like} alt='Like' />
+           </button>
+         </div>
+       </li>
+
+       <li>
+         <img src='https://avatars1.githubusercontent.com/u/2723477?v=4' alt='Avatar' />
+         <footer>
+           <strong>Jonas Porto</strong>
+           <p>Curious, self-starter, troubleshooter and team player. \r\nWorking w/ Cloud, Unix, Ruby, Javascript.\r\nExploring data science possibilities w/ Python, R & Ruby</p>
+         </footer>
+
+         <div className='buttons'>
+           <button type='button'>
+             <img src={dislike} alt='Dislike' />
+           </button>
+           <button type='button'>
+             <img src={like} alt='Like' />
+           </button>
+         </div>
+       </li>
+     </ul>
+   </div>
   )
 }
 
